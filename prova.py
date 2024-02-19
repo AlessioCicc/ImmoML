87ccc411ea050d95ac665d2a2f68255e5a946fd8
Stefano2052 <stefano.ventilii@gmail.com>
2024-02-19 12:18:39 +0100

    Modifica Sidebar.py

--- prova.py
+++ prova.py
@@ -15,8 +15,37 @@ def load_data(n_rows):
 st.set_page_config(page_title='Your App Title', layout='wide')
 
 # Sidebar for user inputs
+#st.sidebar.header('User Input Parameters')
+#number = st.sidebar.number_input('Insert a number', min_value=1, value=10)
+
+# Sidebar for user inputs
 st.sidebar.header('User Input Parameters')
+
+# Original number input
 number = st.sidebar.number_input('Insert a number', min_value=1, value=10)
+
+# New inputs
+# 1. Location Input
+location = st.sidebar.text_input('Enter Location Address')
+
+# 2. Radius Input
+radius = st.sidebar.number_input('Enter Radius', min_value=0.0, value=1.0)
+radius_unit = st.sidebar.selectbox('Select Unit for Radius', ['Kilometers', 'Miles'])
+
+# 3. Number of Rooms Input
+min_rooms, max_rooms = st.sidebar.slider('Select Range of Rooms', 1, 10, (2, 3))
+
+# 4. Space Input
+min_space, max_space = st.sidebar.slider('Select Space Range (in sq meters)', 10, 500, (60, 80))
+space_unit = st.sidebar.selectbox('Select Unit for Space', ['Square Meters', 'Square Feet'])
+
+# Display the inputs
+st.sidebar.write('### Selected Parameters')
+st.sidebar.write(f'Location: {location}')
+st.sidebar.write(f'Radius: {radius} {radius_unit}')
+st.sidebar.write(f'Number of Rooms: {min_rooms} to {max_rooms}')
+st.sidebar.write(f'Space: {min_space} to {max_space} {space_unit}')
+
 
 # Main section
 st.title('Streamlit App delle Trombe')
