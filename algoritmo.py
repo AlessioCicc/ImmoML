d23562cb13072dd307069b59bf62565693095c17
Stefano2052 <stefano.ventilii@gmail.com>
2024-02-29 00:27:55 +0100

    Update algoritmo.py

--- algoritmo.py
+++ algoritmo.py
@@ -6,33 +6,6 @@ def process_data(location=None, min_space=None, max_space=None, min_rooms=None, 
         'location': location if location else "Non specificato",
         # ... altri campi processati
     }
-
-    """
-    Genera dati dummy per una heatmap.
-    
-    Args:
-    lat (float): Latitudine centrale.
-    lon (float): Longitudine centrale.
-    num_points (int): Numero di punti da generare.
-    spread (float): Quanto lontano i punti possono diffondersi dalla posizione centrale.
-
-    Returns:
-    list of dicts: Lista di coordinate e valori.
-    """
-
-    num_points=100
-    spread=0.01
-    
-    data = []
-    for _ in range(num_points):
-        # Genera coordinate casuali intorno alla posizione centrale
-        dummy_lat = lat + random.uniform(-spread, spread)
-        dummy_lon = lon + random.uniform(-spread, spread)
-        # Genera un valore casuale per il punto
-        value = random.uniform(0, 1)
-
-        data.append({'lat': dummy_lat, 'lon': dummy_lon, 'value': value})
-
     return data
 
 
