4cf923c3ff703d8b9453aaafb43e4c361347470d
Stefano2052 <stefano.ventilii@gmail.com>
2024-03-15 16:56:01 +0100

    Update prova.py

--- prova.py
+++ prova.py
@@ -88,70 +88,19 @@ st.sidebar.write('### Parametri Selezionati')
 for key, value in input_dict.items():
     st.sidebar.write(f'{key}: {value}')
 
-# Chiama la funzione process_data
-X = np.array([[max_space, lat, lon, max_bathrooms, max_rooms, condition, floor, elevator, garage],], dtype=object)
-X_norm = preproc.transform(X)
-prezzo = loaded_model.predict(X_norm)
-#processed_data = algoritmo.process_data(location, min_space, max_space, min_rooms, max_rooms, min_bathrooms, max_bathrooms, condition, floor, elevator, garage, energy_efficiency, min_year, max_year)
+# Preparazione e predizione del modello
+X = np.array([[input_dict['Superficie'][1], lat, lon, input_dict['Bagni'][1], 
+               input_dict['Stanze'][1], input_dict['Stato'], input_dict['Piano'], 
+               input_dict['Ascensore'], input_dict['Garage']]], dtype=object)
+prezzo = loaded_model.predict(preproc.transform(X)) if all(X[0]) else None
 
-# Display the inputs
-st.sidebar.write('### Parametri Selezionati')
-st.sidebar.write(f'Località: {location}')
-st.sidebar.write(f'Superficie: da {min_space} a {max_space} mq')
-st.sidebar.write(f'Numero di Stanze: da {min_rooms} a {max_rooms}')
-st.sidebar.write(f'Numero di Bagni: da {min_bathrooms} a {max_bathrooms}')
-st.sidebar.write(f'Stato: {condition}')
-st.sidebar.write(f'Piano: {floor}')
-st.sidebar.write(f'Ascensore: {elevator}')
-st.sidebar.write(f'Garage: {garage}')
-st.sidebar.write(f'Efficienza Energetica: Classe {energy_efficiency}')
-st.sidebar.write(f'Anno di Costruzione: da {min_year} a {max_year}')
-
-# Main section
+# Sezione principale
 st.title('Applicazione Streamlit per Ricerca Immobili')
-
-# Placeholder for Data Display and Further Analysis
-st.write(f'# Prezzo al metro quadro: {int(round(prezzo[0],0)):,} €/m²\n')
-st.write(f'## Prezzo abitazione: {int(round(prezzo[0]*max_space, 0)):,} €')
+if prezzo is not None:
+    st.write(f'# Prezzo al metro quadro: {int(round(prezzo[0],0)):,} €/m²\n')
+    st.write(f'## Prezzo abitazione: {int(round(prezzo[0]*input_dict['Superficie'][1], 0)):,} €')
 
 # Crea una mappa centrata sull'indirizzo specificato
-mappa = folium.Map(location=[lat, lon], zoom_start=13)
-
-# Ottieni i dati dal modello ML
-X = ['surface', 'latitude', 'longitude', 'bathrooms', 'rooms', 'condition', "piano", "ascensore", "garage"]
-#bathrooms              object
-#rooms                  object
-#surface               float64
-#price                 float64
-#latitude              float64
-#longitude             float64
-#condition              object
-#ascensore              object
-#garage                 object
-#piano                 float64
-
-X_norm_list = [] 
-for surface in range(min_space, max_space): 
-    X = np.array([[100, lat, lon, 2, 4, 3, 0, 0, 1],], dtype=object)
-    X_norm = preproc.transform(X)
-    X_norm_list.append(X_norm)
-
-prezzo = loaded_model.predict(X_norm)
-
-
-
-
-# Ottieni i dati per la heatmap
-if location:
-    lat, lon, formatted_address = get_geocode(location)
-    lat, lon = float(lat), float(lon) if lat and lon else (None, None)
-    #if lat and lon:
-        #heatmap_data = algoritmo.generate_dummy_heatmap_data(lat, lon)
-        #heat_data = [[row['lat'], row['lon'], row['value']] for row in heatmap_data] if heatmap_data else []
-
-# Aggiungi la heatmap alla mappa
-#if heat_data:
-#    HeatMap(heat_data).add_to(mappa)
-
-# Visualizza la mappa in Streamlit
-st_folium(mappa, width=700, height=500)
+if all([lat, lon]):
+    mappa = folium.Map(location=[lat, lon], zoom_start=13)
+    st_folium(mappa, width=700, height=500)
