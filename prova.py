29830741c9143ae5d5b2a5614a164092461dea94
AlessioCicc <160173010+AlessioCicc@users.noreply.github.com>
2024-03-21 16:29:52 +0100

    Modifica massiva per usare geocoder

--- prova.py
+++ prova.py
@@ -6,14 +6,18 @@ import requests
 import folium
 from folium.plugins import HeatMap
 from streamlit_folium import st_folium
+from opencage.geocoder import OpenCageGeocode
 import pickle
 import sklearn
+
+key = 'a7bd6bd2c7604ba287219157b6bc946b'
+geocoder = OpenCageGeocode(key)
 
 #dev
 filename = 'model&preproc.pkl'
 with open(filename, 'rb') as infile:
     loaded_model, preproc = pickle.load(infile)
-#X = ['surface', 'latitude', 'longitude', 'bathrooms', 'rooms', 'condition', "piano", "ascensore", "garage"]
+X = ['surface', 'latitude', 'longitude', 'bathrooms', 'rooms', 'condition', "piano", "ascensore", "garage"]
 #bathrooms              object
 #rooms                  object
 #surface               float64
@@ -24,13 +28,15 @@ with open(filename, 'rb') as infile:
 #ascensore              object
 #garage                 object
 #piano                 float64
-#X = np.array([[150.0, 41.876, 12.5776, 2, 4, 3, 0, 0, 1],], dtype=object)
-#X_norm = preproc.transform(X)
-#prezzo = loaded_model.predict(X_norm)
+X = np.array([[150.0, 41.12, 12.5776, 2, 4, 3, 0, 0, 1],], dtype=object)
+X_norm = preproc.transform(X)
+prezzo = loaded_model.predict(X_norm)
 #print(prezzo)
 #fine dev
+
 # Inizializza lat e lon con valori di default
-lat, lon = 41.8797737, 12.4674504  # Posizione di default (es. Roma)
+lat, lon, formatted_address = 41.8797737, 12.4674504, "None"  # Posizione di default (es. Roma)
+
 # Function to load data (dummy example)
 def load_data(n_rows):
     data = pd.DataFrame({
@@ -38,39 +44,44 @@ def load_data(n_rows):
         'second_column': np.random.rand(n_rows)
     })
     return data
-
+    
 # Funzione per ottenere le coordinate geografiche da un indirizzo
 def get_geocode(address):
-    # Qui puoi usare l'API di Google Maps o un'altra API di geocoding
-    # Ad esempio, con OpenStreetMap (Nominatim API):
-    url = f"https://nominatim.openstreetmap.org/search?format=json&q={address}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        if len(data) > 0:
-            lat = data[0]['lat']
-            lon = data[0]['lon']
-            return lat, lon
+    results = geocoder.geocode(query)
+    lat = results[0]['geometry']['lat']
+    lon = results[0]['geometry']['lng']
+    formatted_address = results[0]['formatted']
+    print(u'%f;%f;%s;%s' % (results[0]['geometry']['lat'],
+                            results[0]['geometry']['lng'],
+                            results[0]['components']['country_code'],
+                            results[0]['annotations']['timezone']['name']))
+    return lat, lon, formatted_address
 
 # Streamlit page configuration (optional)
-st.set_page_config(page_title='Your App Title')
-  
+st.set_page_config(page_title='Ricerca Immobili')
+
 # Sidebar for user inputs
 st.sidebar.header('Parametri di Ricerca Immobili')
+
 # 1. Location Input
 location = st.sidebar.text_input('Inserisci Indirizzo', key="address_input")
 
 if location:
-    lat, lon = get_geocode(location)
-    lat, lon = float(lat), float(lon)  # Assicurati che siano float
+    lat, lon, formatted_address = get_geocode(location)
     if lat and lon:
+        lat, lon = float(lat), float(lon)
+    else:
+        lat, lon = None, None
+    if lat and lon and formatted_address:
+        st.sidebar.write(f"{formatted_address}")
         st.sidebar.write(f"Latitudine: {lat}, Longitudine: {lon}")
     else:
         st.sidebar.write("Indirizzo non trovato o non valido")
 
+
 # 2. Space Range Input
 min_space, max_space = st.sidebar.slider('Seleziona Range Superficie (in mq)', 10, 500, (30, 100))
-  
+
 # 3. Number of Rooms Input
 min_rooms, max_rooms = st.sidebar.slider('Seleziona Range Numero di Stanze', 1, 10, (2, 4))
 
@@ -96,7 +107,6 @@ elevator = 1 if elevator_s=="SI" else 0
 # 8. Garage Input
 garage_s = st.sidebar.selectbox('Garage', ['SI', 'NO'])
 garage = 1 if garage_s=="SI" else 0
-
 # 9. Energy Efficiency Range Input
 # Assuming energy efficiency classes range from A to G
 energy_efficiency = st.sidebar.select_slider('Efficienza Energetica', options=['A', 'B', 'C', 'D', 'E', 'F', 'G'])
@@ -113,7 +123,8 @@ prezzo = loaded_model.predict(X_norm)
 
 # Display the inputs
 st.sidebar.write('### Parametri Selezionati')
-st.sidebar.write(f'Località: {location}')
+st.sidebar.write(f'Indirizzo: {formatted_address}')
+st.sidebar.write(f'Lat: {lat}; Lon: {lon}')
 st.sidebar.write(f'Superficie: da {min_space} a {max_space} mq')
 st.sidebar.write(f'Numero di Stanze: da {min_rooms} a {max_rooms}')
 st.sidebar.write(f'Numero di Bagni: da {min_bathrooms} a {max_bathrooms}')
@@ -125,27 +136,29 @@ st.sidebar.write(f'Efficienza Energetica: Classe {energy_efficiency}')
 st.sidebar.write(f'Anno di Costruzione: da {min_year} a {max_year}')
 
 # Main section
-st.title('Applicazione Streamlit per Ricerca Immobili')
+st.title('Applicazione per Ricerca Immobili')
 
 # Placeholder for Data Display and Further Analysis
-st.write(f'Prezzo al metro quadro: {int(round(prezzo[0],0))} € / m²\n Prezzo: {int(round(prezzo[0]*max_space, 0))} €')
+st.write(f'# Prezzo al metro quadro: {int(round(prezzo[0],0)):,} €/m²\n')
+st.write(f'## Prezzo abitazione: {int(round(prezzo[0]*max_space, 0)):,} €')
 
-# Display data on the app
-st.write('### Risultato', 10) #processed_data)
+# Ottieni i dati dal modello ML
+X = ['surface', 'latitude', 'longitude', 'bathrooms', 'rooms', 'condition', "piano", "ascensore", "garage"]
+#bathrooms              object
+#rooms                  object
+#surface               float64
+#price                 float64
+#latitude              float64
+#longitude             float64
+#condition              object
+#ascensore              object
+#garage                 object
+#piano                 float64
 
-# Crea una mappa centrata sull'indirizzo specificato
-mappa = folium.Map(location=[lat, lon], zoom_start=13)
+X_norm_list = [] 
+for surface in range(min_space, max_space): 
+    X = np.array([[100, lat, lon, 2, 4, 3, 0, 0, 1],], dtype=object)
+    X_norm = preproc.transform(X)
+    X_norm_list.append(X_norm)
 
-# Ottieni i dati per la heatmap
-if location:
-    lat, lon = get_geocode(location)
-    if lat and lon:
-        lat, lon = float(lat), float(lon)
-        heatmap_data = algoritmo.generate_dummy_heatmap_data(lat, lon)
-        heat_data = [[row['lat'], row['lon'], row['value']] for row in heatmap_data]
-
-# Aggiungi la heatmap alla mappa
-#HeatMap(heat_data).add_to(mappa)
-
-# Visualizza la mappa in Streamlit
-#st_folium(mappa, width=700, height=500)
+prezzo = loaded_model.predict(X_norm)
