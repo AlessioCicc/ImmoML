9d5b43ba56e613cfbefe49f0cbb190a3f8d0d21d
AlessioCicc <160173010+AlessioCicc@users.noreply.github.com>
2024-03-21 17:19:05 +0100

    Bathrooms_values e rooms_values non sono più liste

--- prova.py
+++ prova.py
@@ -148,9 +148,9 @@ st.write(f'## Prezzo abitazione: {int(round(prezzo[0]*max_space, 0)):,} €')
 # Generazione liste di valori 
 surface_values = np.linspace(min_space, max_space, 3, dtype=int) #tre valori equidistanti all'interno del range tra min_space e max_space
 st.write('Valori di surface_values:', surface_values)
-bathrooms_values = list(range(min_bathrooms, max_bathrooms + 1)) #tutti i valori compresi tra min_bathrooms e max_bathrooms
+bathrooms_values = range(min_bathrooms, max_bathrooms + 1) #tutti i valori compresi tra min_bathrooms e max_bathrooms
 st.write('Valori di bathrooms_values:', bathrooms_values)
-rooms_values = list(range(min_rooms, max_rooms + 1)) #tutti i valori compresi tra min_rooms e max_rooms
+rooms_values = range(min_rooms, max_rooms + 1) #tutti i valori compresi tra min_rooms e max_rooms
 
 X_norm_list = [] 
 for surface in surface_values:
